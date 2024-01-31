e9790a1999cda6478a647311ece7d95f99a644b2
Aman Rawat <amannraawat@gmail.com>
2024-01-31 16:32:43 +0530

    added main function for the organizing/cleaning things

--- cleandesktop.py
+++ cleandesktop.py
@@ -5,3 +5,28 @@ import os
 import json
 import shutil
 
+class Myhandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            i = 1
+            if filename != 'aman':
+                new_name = filename
+                split_name = filename.split('.')
+                file_exists = os.path.isfile(folder_destination + '/' + new_name)
+                while file_exists:
+                    i += 1
+                    new_name = os.path.splittext(folder_to_track + '/' + new_name)[0] + str(i) + os.path.split
+                    new_name = new_name.split('/')[4]
+                    file_exists = os.path.isfile(folder_destination + '/' + new_name)
+                    
+                src = folder_to_track + '/' + filename
+                new_name = folder_destination + '/' + new_name
+                os.rename(src, new_name)
+                
+                
+folder_to_track = '/Users/hp/desktop/example'
+folder_destination = '/Users/hp/desktop/example/aman'
+event_handler = Myhandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
